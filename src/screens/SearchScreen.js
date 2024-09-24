4d7beb07504e34ff6aa3072a9a91e1761c0fcbe9
Nathan Schrader <natecschrader@gmail.com>
2024-09-24 15:09:13 -0700

    add initial parts of SearchScreen, with SearchBar

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,16 +1,28 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, SafeAreaView } from "react-native";
 import SearchBar from "./SearchBar";
 
 const SearchScreen = () => {
+  const [term, setTerm] = useState('');
+
   return (
-    <View>
-      <SearchBar />
+    <View style={styles.background}>
+      <SearchBar 
+        term={term} 
+        onTermChange={(newTerm) => setTerm(newTerm)}
+        onTermSubmit={() => console.log('term submitted')}  
+      />
       <Text>Search Screen</Text>
+      <Text>{term}</Text>
     </View>
   )
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  background: {
+    backgroundColor: 'black',
+    flex: 1,
+  }
+});
 
 export default SearchScreen;
