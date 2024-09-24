376d38848435845850166d4536bf713d24022d7f
Nathan Schrader <natecschrader@gmail.com>
2024-09-24 15:08:09 -0700

    refactor App.js to include global background color, other styling

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import SearchScreen from './src/screens/SearchScreen';
+import { SafeAreaView, StyleSheet } from "react-native";
 
 const navigator = createStackNavigator({
   Search: SearchScreen,
@@ -8,9 +9,26 @@ const navigator = createStackNavigator({
 {
   initialRouteName: 'Search',
   defaultNavigationOptions: {
-    title: 'Business Search'
+    headerShown: false
   }
 }
 )
 
-export default createAppContainer(navigator);
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <AppNavigator />
+      </SafeAreaView>
+  )
+}
+
+const AppNavigator = createAppContainer(navigator);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black', // Set your global background color here
+  },
+});
+
+export default App;
