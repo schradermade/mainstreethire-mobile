90928656b546707f77c85db6922e8122b9f37ae3
Nathan Schrader <natecschrader@gmail.com>
2024-10-07 13:22:17 -0700

    simplified styling, moved styles to ScreenWrapper

--- src/screens/SavedScreen.js
+++ src/screens/SavedScreen.js
@@ -1,21 +1,14 @@
 import React from "react";
-import { StyleSheet } from "react-native";
 import ScreenWrapper from "../components/ScreenWrapper";
 import SavedSection from "../components/Saved";
 
 const SavedScreen = () => {
 
   return (
-    <ScreenWrapper style={styles.container}>
+    <ScreenWrapper>
       <SavedSection />
     </ScreenWrapper>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default SavedScreen;
