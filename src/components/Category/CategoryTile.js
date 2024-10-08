c79f522cad0177021458ba947d9094e0aa560db9
Nathan Schrader <natecschrader@gmail.com>
2024-10-08 12:50:47 -0700

    refactor styling

--- src/components/Category/CategoryTile.js
+++ src/components/Category/CategoryTile.js
@@ -1,36 +1,41 @@
 import React from "react";
-import { borderRadius, colors, fontSize, spacing } from "../../theme/theme";
-import { StyleSheet, Text } from "react-native";
+import { borderRadius, colors, fontSize, iconSize, spacing } from "../../theme/theme";
+import { StyleSheet, Text, View } from "react-native";
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import { TouchableOpacity } from "react-native-gesture-handler";
 
-const CategoryTile = ({ title, iconName, isSelected, onPress }) => {
+const CategoryTile = ({ title, iconName, isSelected, onPress, styling }) => {
   return (
     <TouchableOpacity 
       style={[
         styles.container, 
-        isSelected ? { borderColor: colors.offWhiteFont } : {borderColor: colors.borderColorDark}
+        styling,
+        isSelected && { borderColor: colors.offWhiteFont, borderWidth: 1.5 }
       ]}
       onPress={onPress}
     >
-      {iconName &&
-        <MaterialCommunityIcons name={iconName} style={styles.icon} />
-      }
-      <Text style={styles.titleText}>{title}</Text>
+      <View style={styles.contentContainer}>
+        {iconName &&
+          <MaterialCommunityIcons name={iconName} style={styles.icon} size={iconSize.medium} />
+        }
+        <Text style={styles.titleText}>{title}</Text>
+      </View>
     </TouchableOpacity>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
+    borderWidth: 1,
+    borderColor: colors.borderColorDark,
+    borderRadius: borderRadius.medium,
+    height: 40,
+    justifyContent: 'center',
+  },
+  contentContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    height: 45,
-    borderWidth: 1.5,
-    borderRadius: borderRadius.medium,
-    marginRight: spacing.medium,
-    paddingHorizontal: spacing.medium,
-    marginBottom: spacing.large
+    paddingHorizontal: spacing.small,
   },
   titleText: {
     color: colors.offWhiteFont,
@@ -38,7 +43,6 @@ const styles = StyleSheet.create({
   },
   icon: {
     color: colors.offWhiteFont,
-    fontSize: fontSize.xxlarge,
     marginRight: spacing.small
   },
 })
