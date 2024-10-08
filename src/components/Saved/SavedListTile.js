e19dcbceb8071c36e23faff481afbb7a9c49aa07
Nathan Schrader <natecschrader@gmail.com>
2024-10-08 11:39:30 -0700

    remove explict width/height, control with margin/padding

--- src/components/Saved/SavedListTile.js
+++ src/components/Saved/SavedListTile.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { borderRadius, colors, fontSize, spacing } from "../../theme/theme";
+import { borderRadius, colors, fontSize, iconSize, spacing } from "../../theme/theme";
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import CustomBorder from "../../ui/CustomBorder";
 import { useNavigation } from '@react-navigation/native';
@@ -19,9 +19,9 @@ const SavedListTile = ({ list }) => {
           <View style={styles.iconContainer}>
             <MaterialCommunityIcons name={'map-marker-multiple'} style={styles.icon} />
           </View>
-          <View style={styles.textContainer}>
+          <View>
             <Text style={styles.titleText}>{list.title}</Text>
-            <Text style={styles.numTrailsText}>0 Spottis</Text>
+            <Text style={styles.numSpottisText}>0 Spottis</Text>
           </View>
         </View>
       </TouchableOpacity>
@@ -37,32 +37,29 @@ const SavedListTile = ({ list }) => {
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
-    height: 80
+    padding: spacing.large
   },
   titleText: {
     color: colors.offWhiteFont,
     fontSize: fontSize.large,
     paddingBottom: spacing.xsmall
   },
-  numTrailsText: {
+  numSpottisText: {
     color: colors.darkFont
   },
-  textContainer: {
-    paddingVertical: spacing.large,
-  },
   iconContainer: {
-    padding: spacing.small,
-    margin: spacing.large,
+    padding: spacing.medium,
     backgroundColor: colors.darkGray,
     borderRadius: borderRadius.medium,
-    width: 50,
-    height: 50,
-    justifyContent: 'center',
-    alignItems: 'center'
+    marginRight: spacing.medium,
+    shadowColor: colors.black,
+    shadowOffset: { width: -.5, height: .5 },
+    shadowOpacity: .3,
+    shadowRadius: borderRadius.xsmall,
   },
   icon: {
     color: colors.offWhiteFont,
-    fontSize: 26
+    fontSize: iconSize.medium
   }
 })
 
