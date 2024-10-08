f3d799badd7b99fc74117497ac929a8df3804591
Nathan Schrader <natecschrader@gmail.com>
2024-10-08 12:08:53 -0700

    wip for mapbox implementation

---
+++ src/components/Mapbox/index.js
@@ -0,0 +1,28 @@
+// import React from 'react';
+// import MapboxGL from '@rnmapbox/maps';
+// import { View, StyleSheet } from 'react-native';
+// import { MAPBOX_ACCESS_TOKEN } from '@env';
+
+// MapboxGL.setAccessToken(MAPBOX_ACCESS_TOKEN);
+
+// export default function MapScreen() {
+//   return (
+//     <View style={styles.container}>
+//       <MapboxGL.MapView style={styles.map}>
+//         <MapboxGL.Camera
+//           zoomLevel={8}
+//           centerCoordinate={[-73.935242, 40.730610]}
+//         />
+//       </MapboxGL.MapView>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//   },
+//   map: {
+//     flex: 1,
+//   },
+// });
--- src/screens/NavigateScreen.js
+++ src/screens/NavigateScreen.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import MapScreen from "../components/Mapbox";
 
 const NavigateScreen = () => {
 
   return (
     <View style={styles.background}>
-      <Text>Navigate Screen</Text>
+      <MapScreen />
     </View>
   )
 }
