64008332ea59cdf45119a98fad72af067db48ad1
Nathan Schrader <natecschrader@gmail.com>
2024-10-09 13:57:03 -0700

    build out generic reusable button

---
+++ src/ui/Button.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { colors, fontSize } from "../theme/theme";
+
+const Button = ({ onPress, buttonText, btnStyles }) => {
+  return (
+    <View style={[styles.container, btnStyles]}>
+      <TouchableOpacity style={styles.button} onPress={onPress}>
+        <Text style={styles.buttonText}>{buttonText}</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+  },
+  button: {
+    height: 50,
+    borderRadius: 50,
+    backgroundColor: colors.offWhiteFont,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonText: {
+    fontSize: fontSize.large,
+    color: colors.darkGray
+  }
+})
+
+export default Button;
