d20cb599060dd8db296fea7852b23a4e55e9dcde
Nathan Schrader <natecschrader@gmail.com>
2024-10-11 21:43:55 -0700

    wrap app in safeareaprovider

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
 import 'react-native-gesture-handler';
 import React from "react";
-import { SafeAreaView, StyleSheet } from "react-native";
-import TabsNavigator from "./src/navigation/TabsNavigator";
+import { StyleSheet } from "react-native";
+import AppNavigator from "./src/navigation/AppNavigator";
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { enableScreens } from 'react-native-screens';
 import { colors } from './src/theme/theme';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'; // Updated import
 
 enableScreens();
 
 const App = () => {
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <SafeAreaView style={styles.container}>
-          <TabsNavigator />
-      </SafeAreaView>
-    </GestureHandlerRootView>
+    <SafeAreaProvider>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <SafeAreaView style={styles.container} edges={['top']}>
+            <AppNavigator />
+        </SafeAreaView>
+      </GestureHandlerRootView>
+    </SafeAreaProvider>
   )
 }
 
