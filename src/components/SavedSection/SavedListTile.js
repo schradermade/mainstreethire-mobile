42aaeda70f03dc2818cb0b0d18166eab93258dfd
Nathan Schrader <natecschrader@gmail.com>
2024-10-14 19:17:20 -0700

    Restyled and refactored to more comprehensive card look

--- src/components/SavedSection/SavedListTile.js
+++ src/components/SavedSection/SavedListTile.js
@@ -1,5 +1,9 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import { useNavigation } from "@react-navigation/native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { MaterialIcons } from "@expo/vector-icons";
 import {
   borderRadius,
   colors,
@@ -9,10 +13,7 @@ import {
   fonts,
   shadowRadius,
 } from "../../theme/theme";
-import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
-import CustomBorder from "../../ui/CustomBorder";
-import { useNavigation } from "@react-navigation/native";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import { ICONS } from "../../constants";
 
 const SavedListTile = ({ list }) => {
   const navigation = useNavigation();
@@ -20,27 +21,62 @@ const SavedListTile = ({ list }) => {
   return (
     <>
       <TouchableOpacity
+        style={styles.container}
+        activeOpacity={1}
         key={list.title}
-        onPress={() => navigation.navigate("SavedList", { list })}
+        onPress={() => {
+          console.log("LIST:", list);
+          navigation.navigate("SavedList", { list });
+        }}
       >
-        <View style={styles.container}>
-          <View style={styles.iconContainer}>
-            <MaterialCommunityIcons
-              name={"map-marker-multiple"}
-              style={styles.icon}
-            />
-          </View>
-          <View>
+        <View style={styles.imageWrapper}>
+          <Image
+            source={require("../../../assets/sunriver.jpg")}
+            style={styles.image}
+          />
+        </View>
+        <View style={styles.cardInfoContainer}>
+          <View style={styles.titleContainer}>
             <Text style={styles.titleText}>{list.title}</Text>
-            <Text style={styles.numSpottisText}>0 Spottis</Text>
+
+            <Text style={styles.titleSubText}>0 Spottis</Text>
+          </View>
+          <View style={styles.lineItemContainer}>
+            <View style={styles.lineItem}>
+              <MaterialCommunityIcons
+                name={ICONS.travelDates}
+                color={colors.darkFont}
+                size={iconSize.xsmall}
+              />
+              <Text style={styles.subText}>Jan 4 - Feb 2, '25</Text>
+            </View>
+            <View style={styles.lineItem}>
+              <MaterialIcons
+                name={ICONS.userGroup}
+                color={colors.darkFont}
+                size={iconSize.xsmall}
+              />
+              <Text style={styles.subText}>alexaaaxo, christianJay12</Text>
+            </View>
+            <View style={styles.lineItem}>
+              <MaterialCommunityIcons
+                name={ICONS.travelLocations}
+                color={colors.darkFont}
+                size={iconSize.xsmall}
+              />
+              <Text style={styles.subText}>Germany, Croatia, Italy...</Text>
+            </View>
+            <View style={styles.lineItem}>
+              <MaterialCommunityIcons
+                name={ICONS.categoryList}
+                color={colors.darkFont}
+                size={iconSize.xsmall}
+              />
+              <Text style={styles.subText}>Historical, Famous Streets...</Text>
+            </View>
           </View>
         </View>
       </TouchableOpacity>
-      <CustomBorder
-        borderSize={1}
-        borderColor={colors.borderColorDark}
-        marginHoriz={spacing.large}
-      />
     </>
   );
 };
@@ -48,31 +84,58 @@ const SavedListTile = ({ list }) => {
 const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
-    padding: spacing.large,
+    alignItems: "flex-start",
+    padding: spacing.small,
+    backgroundColor: colors.primaryModalColor,
+    borderRadius: borderRadius.large,
+    borderWidth: 0.2,
+    borderColor: colors.mediumGray,
+    margin: spacing.medium,
+    shadowColor: colors.spottiDark,
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.25,
+    shadowRadius: shadowRadius.xsmall,
+  },
+  titleContainer: {
+    flexDirection: "column",
+  },
+  cardInfoContainer: {
+    paddingLeft: spacing.medium,
+  },
+  lineItemContainer: {
+    flex: 1,
+    justifyContent: "flex-end",
+  },
+  lineItem: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingBottom: spacing.small,
+  },
+  imageWrapper: {
+    shadowColor: colors.white,
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.25,
+    shadowRadius: shadowRadius.xsmall,
+  },
+  image: {
+    borderRadius: borderRadius.large,
+    width: 150,
+    height: 150,
   },
   titleText: {
     color: colors.offWhiteFont,
     fontSize: fontSize.large,
-    paddingBottom: spacing.xsmall,
-    fontFamily: fonts.regular,
+    fontFamily: fonts.semiBold,
   },
-  numSpottisText: {
+  titleSubText: {
+    color: colors.darkFont,
+    fontFamily: fonts.semiBold,
+    fontSize: fontSize.small,
+  },
+  subText: {
     color: colors.darkFont,
     fontFamily: fonts.regular,
-  },
-  iconContainer: {
-    padding: spacing.medium,
-    backgroundColor: colors.darkGray,
-    borderRadius: borderRadius.medium,
-    marginRight: spacing.medium,
-    shadowColor: colors.shadowEffectBlack,
-    shadowOffset: { width: -0.5, height: 0.5 },
-    shadowOpacity: 0.3,
-    shadowRadius: shadowRadius.xsmall,
-  },
-  icon: {
-    color: colors.offWhiteFont,
-    fontSize: iconSize.medium,
+    paddingLeft: spacing.small,
   },
 });
 
