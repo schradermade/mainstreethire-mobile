44432e209443385d8c32ebc24c44abe91fb83583
Nathan Schrader <natecschrader@gmail.com>
2024-10-16 14:24:35 -0700

    build out SpottiMiniTile for a condensed view of Spottis

---
+++ src/components/Spotti/SpottiMiniTile.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { View, Text, StyleSheet } from "react-native";
+import { spacing, colors, fonts } from "../../theme/theme";
+import SpottiQuickFacts from "./SpottiQuickFacts";
+import ImageCarousel from "../ImageCarousel";
+
+const SpottiMiniTile = ({ name, pictures, stats, onPress }) => {
+  return (
+    <TouchableOpacity
+      style={styles.container}
+      onPress={onPress}
+      activeOpacity={1}
+    >
+      <ImageCarousel images={pictures} width={150} />
+      <View style={styles.infoContainer}>
+        <View>
+          <Text style={styles.nameText} numberOfLines={1} ellipsizeMode="tail">
+            {name}
+          </Text>
+          <Text style={styles.subText}>Portland, Oregon</Text>
+        </View>
+        <SpottiQuickFacts
+          stats={stats}
+          textColorTheme="dark"
+          directionAlignment={"vertical"}
+        />
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default React.memo(SpottiMiniTile);
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
+    paddingHorizontal: spacing.large,
+  },
+  infoContainer: {
+    flex: 1,
+    marginLeft: spacing.medium,
+  },
+  nameText: {
+    color: colors.offWhiteFont,
+    fontSize: 20,
+    fontFamily: fonts.semiBold,
+  },
+  subText: {
+    color: colors.darkFont,
+    fontFamily: fonts.regular,
+  },
+});
