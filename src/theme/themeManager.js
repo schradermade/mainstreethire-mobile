b87cce2e09eeb7f3b2674f6f314662a0161df6f8
Nathan Schrader <natecschrader@gmail.com>
2024-10-17 13:13:50 -0700

    build themeManager for alternate component colors, dark vs light

---
+++ src/theme/themeManager.js
@@ -0,0 +1,16 @@
+import { colors } from "./theme";
+
+export const textInputThemes = {
+  light: {
+    inputTextColor: colors.offWhiteFont,
+    inputBackgroundColor: colors.primaryLightColor,
+    labelColor: colors.darkFont,
+    borderColor: colors.borderColorLight,
+  },
+  dark: {
+    inputTextColor: colors.offWhiteFont,
+    inputBackgroundColor: colors.primaryColor,
+    labelColor: colors.darkFont,
+    borderColor: colors.borderColorLight,
+  },
+};
