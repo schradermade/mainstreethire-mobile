345e380200949accefc8501031a0b93b75607ecb
Nathan Schrader <natecschrader@gmail.com>
2024-10-23 12:51:52 -0700

    refactor styling for more consistency in UI

--- src/ui/Divider.js
+++ src/ui/Divider.js
@@ -1,15 +1,20 @@
-import React from "react";
-import { StyleSheet, View } from "react-native";
-import { colors } from "../theme/theme";
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { colors } from '../theme/theme';
 
-const Divider = () => {
-  return <View style={styles.divider} />;
+const Divider = ({ marginT = 0, marginB = 0 }) => {
+  return (
+    <View
+      style={[styles.divider, { marginTop: marginT, marginBottom: marginB }]}
+    />
+  );
 };
 
 const styles = StyleSheet.create({
   divider: {
-    height: 0.5,
-    backgroundColor: colors.borderColorDark || "black",
+    borderBottomWidth: 0.17, // Use borderBottomWidth to achieve an exact 0.2 height line
+    borderBottomColor: colors.borderColorDark || 'black', // Color for the border
+    paddingVertical: 0.5,
   },
 });
 
