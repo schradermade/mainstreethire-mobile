0074d15d23a1ed36cdaa301a15b8cc4c6d2a0a88
Nathan Schrader <natecschrader@gmail.com>
2024-10-25 10:28:04 -0700

    prettier save

--- src/ui/Button.js
+++ src/ui/Button.js
@@ -1,7 +1,7 @@
-import React from "react";
-import { StyleSheet, View, Text } from "react-native";
-import { TouchableOpacity } from "react-native-gesture-handler";
-import { colors, fontSize, fonts, shadowRadius } from "../theme/theme";
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { colors, fontSize, fonts, shadowRadius } from '../theme/theme';
 
 const Button = ({ onPress, buttonText, btnStyles }) => {
   return (
@@ -19,17 +19,17 @@ const Button = ({ onPress, buttonText, btnStyles }) => {
 
 const styles = StyleSheet.create({
   container: {
-    width: "100%",
+    width: '100%',
   },
   button: {
     height: 50,
     borderRadius: 50,
     backgroundColor: colors.offWhiteFont,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
     borderWidth: 1,
     borderColor: colors.buttonActionIconColor,
-    shadowColor: "white",
+    shadowColor: 'white',
     shadowOffset: { width: 0, height: 0 },
     shadowOpacity: 0.35,
     shadowRadius: shadowRadius.xsmall,
