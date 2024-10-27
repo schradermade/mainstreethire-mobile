aaebe8531d0cb8621c9238be8a86c0eed83bb32b
Nathan Schrader <natecschrader@gmail.com>
2024-10-27 11:48:29 -0700

    add spotti slice to redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './slices/userSlice';
 import authReducer from './slices/authSlice';
+import spottiReducer from './slices/spottiSlice';
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
     auth: authReducer,
+    spotti: spottiReducer,
   },
 });
 
