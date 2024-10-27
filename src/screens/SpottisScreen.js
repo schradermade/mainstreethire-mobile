19d3b2e39ba55198c0cb77b75339641a78547f04
Nathan Schrader <natecschrader@gmail.com>
2024-10-27 11:52:51 -0700

    Fetch initial paginated Spottis using Redux dispatch and handle loading, error, and data states

--- src/screens/SpottisScreen.js
+++ src/screens/SpottisScreen.js
@@ -1,21 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Text } from 'react-native';
 import SearchBar from '../components/SpottiSection/SearchBar';
-import spotti from '../api/spotti';
 import CategoryList from '../components/SpottiSection/Category/CategoryList';
 import SpottiList from '../components/Spotti/SpottiList';
 import ScreenWrapper from '../components/ScreenWrapper';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import SpottiTile from '../components/Spotti/SpottiTile';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchSpottis } from '../redux/slices/spottiSlice';
 
 const SpottiScreen = () => {
   const [term, setTerm] = useState('');
-  const [spottis, setSpottis] = useState([]);
-  const insets = useSafeAreaInsets();
 
-  const spottiApi = async () => {
-    const response = await spotti.get();
-    setSpottis(response.data);
+  const insets = useSafeAreaInsets();
+  const dispatch = useDispatch();
+  const {
+    data: spottis,
+    loading,
+    error,
+  } = useSelector((state) => state.spotti);
+
+  useEffect(() => {
+    dispatch(fetchSpottis({ limit: 10, offset: 0 }));
+  }, [dispatch]);
+
+  const loadMoreSpottis = () => {
+    dispatch(fetchSpottis({ limit: 10, offset: spottis.length }));
   };
+
+  if (loading) {
+    return <Text>Loading...</Text>;
+  }
+
+  if (error) {
+    return <Text>Error loading Spottis: {error.messages || error}</Text>;
+  }
 
   return (
     <ScreenWrapper
@@ -26,7 +45,7 @@ const SpottiScreen = () => {
       <SearchBar
         term={term}
         onTermChange={(newTerm) => setTerm(newTerm)}
-        onTermSubmit={spottiApi}
+        // onTermSubmit={spottiApi}
       />
       <CategoryList />
       <SpottiList spottis={spottis} TileComponent={SpottiTile} />
