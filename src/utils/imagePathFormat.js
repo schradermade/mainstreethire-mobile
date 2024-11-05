88fe2d216329fef12959ec24fbb3e7544e60bdc7
Nathan Schrader <natecschrader@gmail.com>
2024-11-04 16:18:22 -0800

    add logic to catch undefined images

--- src/utils/imagePathFormat.js
+++ src/utils/imagePathFormat.js
@@ -1,5 +1,5 @@
 export const imagePathFormat = (images) => {
-  return images.map((image) =>
+  return (images || []).map((image) =>
     typeof image === 'string' ? { uri: image } : image
   );
 };
