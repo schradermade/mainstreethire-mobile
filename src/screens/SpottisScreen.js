296e478fbc80be9d15aa01f418f7d778d429787c
Nathan Schrader <natecschrader@gmail.com>
2024-11-04 16:19:07 -0800

    refactor loading view with LoadingSpinner comp

--- src/screens/SpottisScreen.js
+++ src/screens/SpottisScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 import SearchBar from '../components/SpottiSection/SearchBar';
 import CategoryList from '../components/SpottiSection/Category/CategoryList';
 import SpottiList from '../components/Spotti/SpottiList';
@@ -8,6 +8,7 @@ import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import SpottiTile from '../components/Spotti/SpottiTile';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchSpottis } from '../redux/slices/spottiSlice';
+import LoadingSpinner from '../components/LoadingSpinner';
 
 const SpottiScreen = () => {
   const [term, setTerm] = useState('');
@@ -20,6 +21,8 @@ const SpottiScreen = () => {
     error,
   } = useSelector((state) => state.spotti);
 
+  console.log('SPOTTIS_IN_SLICE!!:', JSON.stringify(spottis, null, 2));
+
   useEffect(() => {
     dispatch(fetchSpottis({ limit: 20, offset: 0 }));
   }, [dispatch]);
@@ -27,14 +30,6 @@ const SpottiScreen = () => {
   const loadMoreSpottis = () => {
     dispatch(fetchSpottis({ limit: 10, offset: spottis.length }));
   };
-
-  if (loading) {
-    return <Text>Loading...</Text>;
-  }
-
-  if (error) {
-    return <Text>Error loading Spottis: {error.messages || error}</Text>;
-  }
 
   return (
     <ScreenWrapper
@@ -48,7 +43,13 @@ const SpottiScreen = () => {
         // onTermSubmit={spottiApi}
       />
       <CategoryList />
-      <SpottiList spottis={spottis} TileComponent={SpottiTile} />
+      {error ? (
+        <Text>Error loading Spottis: {error.messages || error}</Text>
+      ) : loading ? (
+        <LoadingSpinner label={'Loading Spottis...'} />
+      ) : (
+        <SpottiList spottis={spottis} TileComponent={SpottiTile} />
+      )}
     </ScreenWrapper>
   );
 };
